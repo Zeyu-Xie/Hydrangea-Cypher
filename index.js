daa528341050027d988165fbf071b039fec3a8e7
Acan <xie.zeyu20@gmail.com>
2023-10-19 20:54:50 +0800

    .

--- index.js
+++ index.js
@@ -2,20 +2,18 @@ const Sha256 = CryptoJS.SHA256
 const Aes = CryptoJS.AES
 const Enc = CryptoJS.enc
 
-const _contentInput = document.querySelector("input[id='contentInput']")
+const _contentInput = document.querySelector("textarea[id='contentInput']")
 const _passphraseInput = document.querySelector("input[id='passphraseInput']")
 const _encryptButton = document.querySelector("button[id='encryptButton']")
 const _decryptButton = document.querySelector("button[id='decryptButton']")
-const _result = document.querySelector("textarea[id='result']")
-
-let content = _contentInput.value
-let passphrase = _passphraseInput.value
+const _copyButton = document.querySelector("button[id='copyButton']")
+const _result = document.querySelector("div[id='result']")
 
 _encryptButton.addEventListener("click", () => {
     const content = _contentInput.value
     const passphrase = _passphraseInput.value
     const encryptedContent = Aes.encrypt(content, passphrase).toString()
-    _result.value = encryptedContent
+    _result.innerText = encryptedContent
 })
 
 _decryptButton.addEventListener("click", () => {
@@ -23,6 +21,19 @@ _decryptButton.addEventListener("click", () => {
     const passphrase = _passphraseInput.value
     const decryptedBytes = Aes.decrypt(content, passphrase)
     const decryptedContent = decryptedBytes.toString(Enc.Utf8)
-    // console.log(decryptedContent)
-    _result.value = decryptedContent
+    _result.innerText = decryptedContent
+})
+
+_copyButton.addEventListener("click", () => {
+    const clipboardItem = new ClipboardItem({ "text/plain": new Blob([_result.innerText]) });
+    navigator.clipboard.write([clipboardItem]).then(
+        function () {
+            window.alert("Copy Successfully")
+            console.log("Copy Successfully")
+        },
+        function (err) {
+            window.alert("Copy Failed")
+            console.error("ERROR", err)
+        }
+    )
 })
